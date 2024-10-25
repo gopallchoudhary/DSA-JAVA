b1266fbfbce27c5566bb8343ffe0e6037ca2e37f
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-10-25 10:12:17 +0530

    added multiplication table

---
+++ Questions/Loops/MultiplicationTable.java
@@ -0,0 +1,17 @@
+package Questions.Loops;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to print its TABLE: ");
+        int num = sc.nextInt();
+        for(int i = 1; i<=10; i++) {
+            System.out.println(num * i);
+        }
+
+
+        sc.close();
+    }
+}
