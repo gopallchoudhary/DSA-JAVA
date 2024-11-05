e038737cb5a3d04b0211fdfb4c0e8a8e5f0baa1b
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-06 00:26:34 +0530

    added inverted half pyramid

--- Pattern.java
+++ Pattern.java
@@ -30,12 +30,22 @@ public class Pattern {
         /// Character Pattern
         // char ch = 'A';
         // for (int i = 1; i <= 4; i++) {
-        //     for (int j = 1; j <= i; j++) {
-        //         System.out.print(ch);
-        //         ch++;
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(ch);
+        // ch++;
+        // }
+        // System.out.println();
+        // }
+
+        /// Inverted Half Pyramid 
+        // int a = 1;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 5; j >= i; j--) {
+        //         System.out.print(a);
+        //         a++;
         //     }
         //     System.out.println();
+        //     a=1;
         // }
-        
     }
 }
