e578d8b3965215e73bdc084d4ca5388f10f1d8bc
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-05 23:48:36 +0530

    added character pattern

--- Pattern.java
+++ Pattern.java
@@ -8,24 +8,34 @@ public class Pattern {
 
         // }
         // System.out.println();
-        // } 
+        // }
 
-        /// Inverted Star 
-        
+        /// Inverted Star
+
         // for (int i = 1; i <= 5; i++) {
-        //     for (int j = 5; j >= i; j--) {
-        //         System.out.print("* ");
+        // for (int j = 5; j >= i; j--) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        /// Half-Pyramid
+        // for (int i = 1; i <=4; i++) {
+        // for (int j = 1; j <=i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        /// Character Pattern
+        // char ch = 'A';
+        // for (int i = 1; i <= 4; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(ch);
+        //         ch++;
         //     }
         //     System.out.println();
         // }
-
-
-        /// Half-Pyramid  
-        for (int i = 1; i <=4; i++) {
-            for (int j = 1; j <=i; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
+        
     }
 }
