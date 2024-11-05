47d9eb64826c932c08006203c51831c53112a72c
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-05 20:13:14 +0530

    added half pyramid

--- Pattern.java
+++ Pattern.java
@@ -1,19 +1,29 @@
 public class Pattern {
 
     public static void main(String[] args) {
-        /// STAR  
+        /// STAR
         // for (int i = 1; i <= 5; i++) {
-        //     for (int j = 1; j <=i; j++) {
+        // for (int j = 1; j <=i; j++) {
+        // System.out.print("* ");
+
+        // }
+        // System.out.println();
+        // } 
+
+        /// Inverted Star 
+        
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 5; j >= i; j--) {
         //         System.out.print("* ");
-                
         //     }
         //     System.out.println();
         // }
 
-        /// Inverted Star 
-        for (int i = 1; i <=5; i++) {
-            for (int j = 5; j >=i; j--) {
-                System.out.print("* ");
+
+        /// Half-Pyramid  
+        for (int i = 1; i <=4; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
