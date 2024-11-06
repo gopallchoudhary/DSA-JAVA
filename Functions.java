978481fb98029ed71eb1d47c27bd81c7589f9fdb
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-06 15:42:17 +0530

    added functions

---
+++ Functions.java
@@ -0,0 +1,12 @@
+public class Functions {
+    public static void printHW() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return;
+    }
+
+    public static void main(String[] args) {
+        printHW();
+    }
+}
