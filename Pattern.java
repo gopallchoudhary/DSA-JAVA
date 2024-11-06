435d0998cad72fc64d05c9749aeed19115933656
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-06 08:15:50 +0530

    added floyds triangle

--- Pattern.java
+++ Pattern.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Pattern {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         /// STAR
         // for (int i = 1; i <= 5; i++) {
         // for (int j = 1; j <=i; j++) {
@@ -37,15 +41,31 @@ public class Pattern {
         // System.out.println();
         // }
 
-        /// Inverted Half Pyramid 
+        /// Inverted Half Pyramid
         // int a = 1;
         // for (int i = 1; i <= 5; i++) {
-        //     for (int j = 5; j >= i; j--) {
-        //         System.out.print(a);
-        //         a++;
-        //     }
-        //     System.out.println();
-        //     a=1;
+        // for (int j = 5; j >= i; j--) {
+        // System.out.print(a);
+        // a++;
         // }
+        // System.out.println();
+        // a=1;
+        // }
+
+        /// Floyds-Triangle
+        // System.out.println("Enter number of rows for floyds tringle");
+        // int row = sc.nextInt();
+        // int a = 1;
+        // for (int i = 1; i <= row; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(a + " ");
+        // a++;
+        // }
+        // System.out.println();
+        // }
+
+
+        
+        sc.close();
     }
 }
