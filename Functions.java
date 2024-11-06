c55cafdc337948dd5233cf4997cf1578479fbc1e
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-06 16:01:33 +0530

    added functions with paramaters

--- Functions.java
+++ Functions.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 public class Functions {
+    //.<==Function without params==> 
     public static void printHW() {
         System.out.println("Hello World");
         System.out.println("Hello World");
@@ -6,7 +9,22 @@ public class Functions {
         return;
     }
 
+    //.<==Function with params==> 
+    public static int calculateSum(int n1, int n2) {
+        int sum = n1 + n2;
+        return sum;
+        
+    }
+
     public static void main(String[] args) {
-        printHW();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        int sum = calculateSum(a, b); //! sum function
+        System.out.println("Sum is:  "+sum);
+
+        sc.close();
     }
 }
