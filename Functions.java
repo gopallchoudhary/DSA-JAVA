bacfd5d79e86d05dbafb06cb9aef65a17ee99437
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-07 06:32:29 +0530

    added swap numbers

--- Functions.java
+++ Functions.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Functions {
-    //.<==Function without params==> 
+    // .<==Function without params==>
     public static void printHW() {
         System.out.println("Hello World");
         System.out.println("Hello World");
@@ -9,21 +9,36 @@ public class Functions {
         return;
     }
 
-    //.<==Function with params==> 
-    public static int calculateSum(int n1, int n2) {
+    // .<==Function with params==>
+    public static int calculateSum(int n1, int n2) { // ! n1 & n2 are parameters or formal parameters
         int sum = n1 + n2;
         return sum;
-        
+
     }
 
+    
+    // .<==Swap Numbers
+    public static void swap(int a, int b) {
+        //swap
+        int temp = a; 
+        a = b;
+        b = temp;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter second number:");
-        int b = sc.nextInt();
-        int sum = calculateSum(a, b); //! sum function
-        System.out.println("Sum is:  "+sum);
+       // System.out.println("Enter first number:");
+        //int a = sc.nextInt();
+       // System.out.println("Enter second number:");
+        //int b = sc.nextInt();
+        //int sum = calculateSum(a, b); //! a & b are arguments or actual parameters
+        //System.out.println("Sum is:  "+sum);
+
+        /// SWAP-NUMBERS  
+        int a = 5;
+        int b = 10;
+        swap(a, b);
 
         sc.close();
     }
