03be4dc627e8da2c7c2565c0be7f86f94a141558
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-07 07:01:31 +0530

    added factorial-functions

--- Functions.java
+++ Functions.java
@@ -16,29 +16,58 @@ public class Functions {
 
     }
 
-    
     // .<==Swap Numbers
     public static void swap(int a, int b) {
-        //swap
-        int temp = a; 
+        // swap
+        int temp = a;
         a = b;
         b = temp;
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
+
+    // .<==Product==>
+    public static int multiply(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
+    // .<==Factorial==>
+    public static int factorial(int n) {
+        int factorial = 1;
+        for (int i = 2; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       // System.out.println("Enter first number:");
-        //int a = sc.nextInt();
-       // System.out.println("Enter second number:");
-        //int b = sc.nextInt();
-        //int sum = calculateSum(a, b); //! a & b are arguments or actual parameters
-        //System.out.println("Sum is:  "+sum);
+        // System.out.println("Enter first number:");
+        // int a = sc.nextInt();
+        // System.out.println("Enter second number:");
+        // int b = sc.nextInt();
+        // int sum = calculateSum(a, b); //! a & b are arguments or actual parameters
+        // System.out.println("Sum is: "+sum);
 
-        /// SWAP-NUMBERS  
-        int a = 5;
-        int b = 10;
-        swap(a, b);
+        /// SWAP-NUMBERS
+        // int a = 5;
+        // int b = 10;
+        // swap(a, b);
+
+        /// Product of Numbers
+        // int a = 5;
+        // int b = 10;
+        // int prod = multiply(a, b);
+        // System.out.println("Product of a and b is " + prod);
+        // prod = multiply(5, 20);
+        // System.out.println("Product of a and b is " + prod);
+
+        /// Factorial
+        System.out.println("Enter a number to find the factorial");
+        int n = sc.nextInt();
+        int fact = factorial(n);
+        System.out.println("Factorial of a " + n + " is: " + fact);
 
         sc.close();
     }
