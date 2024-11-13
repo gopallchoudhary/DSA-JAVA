1babdc64c965007b76fd34fb4a6a8ad028ee402a
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-13 17:40:49 +0530

    added swaping of two no.s

--- JavaBasics.java
+++ JavaBasics.java
@@ -4,9 +4,15 @@ public class JavaBasics {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int a = 5;
+        int b = 10;
 
-        
+        //swap
+        int temp = a; 
+        a = b;
+        b = temp;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
         
 
         
