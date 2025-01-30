9562e0206fb4829ea268132085d14115b3780122
gopallchoudhary <gopalchoudhary521@gmail.com>
2025-01-30 23:14:55 +0530

    solved kadanes algorithm

---
+++ Arrays/kadanesAlgo.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class kadanesAlgo {
+
+    public static void maxSumArray(int numbers[]) {
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        System.out.print("Current Sum: ");
+        for(int i=0; i<numbers.length; i++) {
+            currentSum+=numbers[i];
+            
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
+            
+            System.out.print(currentSum+", ");
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        System.out.println();
+        System.out.println("Max sum: "+ maxSum);
+    }
+    public static void main(String[] args) {
+        int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
+        maxSumArray(numbers);
+
+    }
+}
