da2ca7f5d6519d4b05b7bbf0e8dc4e75385aedfa
gopallchoudhary <gopalchoudhary521@gmail.com>
2025-01-30 23:14:19 +0530

    solved max of sum of sub array

---
+++ Arrays/maxSubArray.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+public class maxSubArray {
+
+    public static void maaxSubArray(int numbers[]) {
+        int size = numbers.length;
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int minSum = Integer.MAX_VALUE;
+
+        System.out.print("Current Sum: ");
+
+        for(int i=0; i<size; i++) {
+            for(int j=i; j<size; j++) {
+                currentSum = 0;
+                for(int k=i; k<=j; k++) {
+                    //System.out.print(numbers[k]+" ");
+                    currentSum+=numbers[k];
+                }
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    
+                }
+
+                if(currentSum < minSum) {
+                    minSum = currentSum;
+                }
+                System.out.print(currentSum+", ");
+            }
+
+        }
+        System.out.println();
+        System.out.println("Maximum sub array is: "+ maxSum);
+        System.out.println("Minimum sub array is: "+ minSum);
+
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        maaxSubArray(numbers);
+
+    }
+}
